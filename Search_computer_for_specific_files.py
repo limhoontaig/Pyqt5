8ed2b925cb45b93d7d139bff207cee31e69a0780
limhoontaig <limhoontaig@gmail.com>
2021-04-18 08:25:33 +0900

    error collection from \ to / SyntaxError: EOL while scanning string literal

--- Search_computer_for_specific_files.py
+++ Search_computer_for_specific_files.py
@@ -4,7 +4,8 @@ import os
 images = ['*.jpg', '*.jpeg', '*.png', '*.tif', '*.tiff']
 matches = []
 
-for root, dirnames, filenames in os.walk("C:\"):
+for root, dirnames, filenames in os.walk("C:/CCTV/"):
     for extensions in images:
         for filename in fnmatch.filter(filenames, extensions):
             matches.append(os.path.join(root, filename))
+print(matches)
