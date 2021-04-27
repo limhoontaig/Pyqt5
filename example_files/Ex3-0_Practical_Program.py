f38ed8097491047c4f514839ea8b8ec2f689be7e
limhoontaig <limhoontaig@gmail.com>
2021-04-27 20:29:18 +0900

    Add Grid Function

--- example_files/Ex3-0_Practical_Program.py
+++ example_files/Ex3-0_Practical_Program.py
@@ -1,9 +1,9 @@
 ## Ex 3-1. 창 띄우기.
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QLabel, QLineEdit, QTextEdit
 from PyQt5.QtGui import QIcon
-
+from PyQt5.QtCore import QCoreApplication
 
 class MyApp(QWidget):
 
@@ -17,6 +17,23 @@ class MyApp(QWidget):
         self.move(300, 300)
         self.resize(400, 200)
         # self.setGeometry(300, 300, 300, 200)
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        grid.addWidget(QLabel('Title:'), 0, 0)
+        grid.addWidget(QLabel('Author:'), 1, 0)
+        grid.addWidget(QLabel('Review:'), 2, 0)
+        grid.addWidget(QLabel('Quit:'), 3, 0)
+
+        grid.addWidget(QLineEdit(), 0, 1)
+        grid.addWidget(QLineEdit(), 1, 1)
+        grid.addWidget(QTextEdit(), 2, 1)
+        grid.addWidget(QPushButton(), 3, 1)
+
+        btn = QPushButton('Quit', self)
+        btn.move(50, 50)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(QCoreApplication.instance().quit)
         self.show()
 
 
