1f1b0f30d16f5d1735e453a1ad362c5781c73f0f
limhoontaig <limhoontaig@gmail.com>
2021-05-05 19:24:10 +0900

    add morse function

--- wikidocs_examples.py
+++ wikidocs_examples.py
@@ -198,20 +198,39 @@
 
 # Example 14 Duplicate numbers
 
-def chkDupNum(s):
-    result = []
-    for num in s:
-        if num not in result:
-            result.append(num)
-            #print(result)
-        else:
-            return False
-    #print(result)
-    return len(result) == 10
+# def chkDupNum(s):
+#     result = []
+#     for num in s:
+#         if num not in result:
+#             result.append(num)
+#             #print(result)
+#         else:
+#             return False
+#     #print(result)
+#     return len(result) == 10
 
-print(chkDupNum('0123456789'))
-print(chkDupNum('01234'))
-print(chkDupNum('01234567890'))
-print(chkDupNum('6789012345'))
-print(chkDupNum('012322456789'))
-        
+# print(chkDupNum('0123456789'))
+# print(chkDupNum('01234'))
+# print(chkDupNum('01234567890'))
+# print(chkDupNum('6789012345'))
+# print(chkDupNum('012322456789'))
+
+# Example 16 Morse
+
+dic = {
+    '.-':'A','-...':'B','-.-.':'C','-..':'D','.':'E','..-.':'F',
+    '--.':'G','....':'H','..':'I','.---':'J','-.-':'K','.-..':'L',
+    '--':'M','-.':'N','---':'O','.--.':'P','--.-':'Q','.-.':'R',
+    '...':'S','-':'T','..-':'U','...-':'V','.--':'W','-..-':'X',
+    '-.--':'Y','--..':'Z'
+}
+
+def morse(src):
+    result = []
+    for word in src.split('  '):
+        for char in word.split(' '):
+            result.append(dic[char])
+        result.append(' ')
+    return ''.join(result)
+
+print(morse('.... .  ... .-.. . . .--. ...  . .- .-. .-.. -.--'))
