5077f38f614b27bf3dfe9a8aa126bbb430e86fb5
limhoontaig <limhoontaig@gmail.com>
2021-05-07 07:16:18 +0900

    Add tuple method

--- ListComprehension.py
+++ ListComprehension.py
@@ -15,3 +15,20 @@ stocknames = [s['name'] for s in stocks]
 cost = sum([s['price']*s['stock'] for s in stocks])
 print(stocknames)
 print(cost)
+
+for s in stocks:
+    #print(s)
+    print('{name:<10s} {price:>10.2f} {stock:>10d}' .format_map(s))# s in stocks)
+
+headers = ('name', 'price','stock'),
+stocks = [
+    ('a',50,20),
+    ('abc',700,25),
+    ('ba',50,20),
+    ('cba',50,20)
+]
+
+stocknames = [s[0] for s in stocks]
+cost = sum(s[1]*s[2] for s in stocks)
+print(stocknames)
+print(cost)
