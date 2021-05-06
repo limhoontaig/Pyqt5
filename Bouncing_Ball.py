b1e445981f02309d07bf64bb8543a9bd681c7cbd
limhoontaig <limhoontaig@gmail.com>
2021-05-06 16:40:07 +0900

    added filename and calculation method change

--- Bouncing_Ball.py
+++ Bouncing_Ball.py
@@ -1,8 +1,11 @@
+# E:\source\pyqt5\Bouncing_Ball.py
 #height = 0.0
 #i=0
 height = float(input("input your initial height: "))
 for i in range(15):
     
     print (i, height)
+    #height = 0.6 * height
     height *= 0.6
+
 
