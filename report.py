3ef09056732f9343de1d1470e4c6b93e4c9558e6
limhoontaig <limhoontaig@gmail.com>
2021-05-06 17:18:21 +0900

    now ongoing

---
+++ report.py
@@ -0,0 +1,33 @@
+# E:\source\pyqt5\report.py
+
+import csv
+
+def read_portfolio(filename):
+    '''
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price.
+    '''
+    portfolio = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+
+        for row in rows:
+            record = dict(zip(headers, row))
+            stock = {
+                'name' : record['name'],
+                'shares' : int(record['shares']),
+                'price' : float(record['price'])
+            }
+            portfolio.append(stock)
+
+    return portfolio
+
+# output the report
+
+headers = ('Name', 'Shares', 'Price', 'Change')
+print('%10s %10s %10s %10s' % headers)
+print(('-' * 10 + ' ') * len(headers))
+for row in report:
+    print('%10s %10d %10.2f %10.2f' % row)
+
