c7204deff60afae658ee45e3442d680814ba3d22
limhoontaig <limhoontaig@gmail.com>
2021-05-08 11:58:13 +0900

    add monthly payplan and cumulative payment status

--- mortage.py
+++ mortage.py
@@ -1,22 +1,27 @@
 # E:\source\pyqt5\mortage.py
 
-principal = 500000.0
+principal = 200000.0
 rate = 0.05
 payment = 2684.11
 extra_payment = 1000.0
+monthly_paid = 0
 total_paid = 0.0
 month = 0
+h = ['month','principal','monthly_paid','monthly_rate','monthly_total','total_paid','remain_principal']
+print(f'{h[0]:>5s} {h[1]:>12s} {h[2]:>12s} {h[3]:>12s} {h[4]:>12s} {h[5]:>12s} {h[6]:>12s}')
 
 while principal > 0:
     month +=1
+    monthly_rate = principal * rate / 12
+    monthly_paid = payment - monthly_rate
     if month > 60:
         principal = principal * (1+rate/12) - payment - extra_payment
         total_paid = total_paid + payment + extra_payment
-        print(month, principal, total_paid)
+        print(f'{month:>5d} {principal+monthly_paid:>12.2f} {monthly_paid:>12.2f} {monthly_rate:>12.2f} {monthly_paid+monthly_rate:>13.2f} {total_paid:>12.2f} {principal:>16.2f}')
 
     else:
         principal = principal * (1+rate/12) - payment
         total_paid = total_paid + payment
-        print(month, principal, total_paid)
+        print(f'{month:>5d} {principal+monthly_paid:>12.2f} {monthly_paid:>12.2f} {monthly_rate:>12.2f} {monthly_paid+monthly_rate:>13.2f} {total_paid:>12.2f} {principal:>16.2f}')
 
-print('Total paid', total_paid)
+print('Total paid', f'{total_paid:>12.2f}')
