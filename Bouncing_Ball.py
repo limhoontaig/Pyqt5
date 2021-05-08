9a6d670e5a6a85b800abe50e4ed1f7b4a3dd1c5d
limhoontaig <limhoontaig@gmail.com>
2021-05-08 10:33:32 +0900

    Modified height's unit and printing format using f string

--- Bouncing_Ball.py
+++ Bouncing_Ball.py
@@ -1,10 +1,10 @@
 # E:\source\pyqt5\Bouncing_Ball.py
 #height = 0.0
 #i=0
-height = float(input("input your initial height: "))
+height = float(input("input your initial height(meters): "))
 for i in range(15):
     
-    print (i, height)
+    print (f'{i:>3d} {height:>8.2f}')
     #height = 0.6 * height
     height *= 0.6
 
