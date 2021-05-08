6df5851776f195b55dbc8f9de264512105217b99
limhoontaig <limhoontaig@gmail.com>
2021-05-08 13:23:42 +0900

    add extra payment duration

--- mortage.py
+++ mortage.py
@@ -1,8 +1,11 @@
 # E:\source\pyqt5\mortage.py
 
-principal = 200000.0
+principal = 500000.0
 rate = 0.05
 payment = 2684.11
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
 extra_payment = 1000.0
 monthly_paid = 0
 total_paid = 0.0
@@ -14,7 +17,7 @@ while principal > 0:
     month +=1
     monthly_rate = principal * rate / 12
     if principal > payment + extra_payment:
-        if month > 60:
+        if month >= extra_payment_start_month and month <= extra_payment_end_month:
             principal = principal * (1+rate/12) - payment - extra_payment
             monthly_paid = payment + extra_payment - monthly_rate
             total_paid += payment + extra_payment
