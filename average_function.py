411b9f8f324bff9a3b854c41d12322d063957c60
limhoontaig <limhoontaig@gmail.com>
2021-05-09 10:01:43 +0900

    not operated function for list

--- average_function.py
+++ average_function.py
@@ -6,6 +6,6 @@ def avg_numbers(*args):
 
 avg = avg_numbers(1,2,3,4,250005)
 print('avg = avg_numbers(1,2,3,4,250005)', avg)
-l = (1,3,5,7,9,11,13,15,17,19,20)
-avg = avg_numbers(l)
-print('l = [1,3,5,7,9,11,13,15,17,19,20]', avg)
+
+# avg = avg_numbers(1,3,5,7,9,11,13,15,17,19,20)
+# print('l = [1,3,5,7,9,11,13,15,17,19,20]', avg)
