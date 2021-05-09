d64f9c670a9f520902b03d32f7b2653d62d04d43
limhoontaig <limhoontaig@gmail.com>
2021-05-09 16:11:25 +0900

    import csv sys with try except also

---
+++ work/exam_01_32.py
@@ -0,0 +1,23 @@
+import sys
+import csv
+
+def portfolio_cost(filename):
+    total_cost = 0
+    f = open(filename)
+    rows = csv.reader(f)
+    headers = next(rows)
+    for row in rows:
+        try:
+            total_cost = int(row[1])*float(row[2])
+        except ValueError:
+            print('Value Error :', row)
+
+    return total_cost
+            
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'data/missing.csv'
+
+total_cost = portfolio_cost(filename)
+print('Total cost :',total_cost)
