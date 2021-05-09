c7e74999f80972d08617bd4b65ce8745065624d0
limhoontaig <limhoontaig@gmail.com>
2021-05-09 19:22:58 +0900

    added for total cost computation

--- work/chap_02_02.py
+++ work/chap_02_02.py
@@ -1,24 +1,28 @@
 import sys
 import csv
 
-def portfolio_cost(filename):
+def portfolio_list(filename):
     records = []
+    total_cost = 0
     f = open(filename)
     rows = csv.reader(f)
     headers = next(rows)
     for row in rows:
         records.append((row[0],int(row[1]),float(row[2])))
-        # try:
-        #     total_cost = d['shares']*d['price']
-        # except ValueError:
-        #     print('Value Error :', row)
+        try:
+            total_cost = int(row[1])*float(row[2])
+        except ValueError:
+            print('Value Error :', row)
         
-    return records
+    return records, total_cost
             
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
     filename = 'data/portfolio.csv'
 
-total_list = portfolio_cost(filename)
-print('Total list :', total_list)
+total = portfolio_list(filename)
+print()
+print('Total list :', total[0])
+print()
+print('Total cost :', total[1])
