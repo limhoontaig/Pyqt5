8c07d108a8e4de6fabbaf7f2d6ff3e81fe1cd199
limhoontaig <limhoontaig@gmail.com>
2021-05-10 20:09:50 +0900

    make a portfolio_cost function

--- work/pcost.py
+++ work/pcost.py
@@ -1,12 +1,20 @@
-total_cost = 0
+# C:\source\PyQt5\work\pcost.py
 
-with open('data/portfolio.csv', 'rt') as f:
-    headers = next(f).split(',')
-    for line in f:
-        row = line.split(',')
-        try:
-            total_cost += int(row[1])*float(row[2])
-        except ValueError:
-            print("Couldn't Parse", line)
-    print(total_cost)
-    
+def portfolio_cost(filename):
+
+    total_cost = 0
+
+    with open(filename, 'rt') as f:
+        headers = next(f).split(',')
+        for line in f:
+            row = line.split(',')
+            try:
+                total_cost += int(row[1])*float(row[2])
+            except ValueError:
+                print("Couldn't Parse", line)
+    return(total_cost)
+
+cost = portfolio_cost('data/portfolio.csv')
+print('Total cost :',cost)
+
+        
