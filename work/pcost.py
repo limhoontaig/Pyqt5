b9aad37648fef19ff2ee706b15fef8e9c2294286
limhoontaig <limhoontaig@gmail.com>
2021-05-10 20:22:07 +0900

    modified using csv module

--- work/pcost.py
+++ work/pcost.py
@@ -1,17 +1,19 @@
 # C:\source\PyQt5\work\pcost.py
 
+import csv
+
 def portfolio_cost(filename):
 
     total_cost = 0
 
-    with open(filename, 'rt') as f:
-        headers = next(f).split(',')
-        for line in f:
-            row = line.split(',')
-            try:
-                total_cost += int(row[1])*float(row[2])
-            except ValueError:
-                print("Couldn't Parse", line)
+    f = open(filename)
+    rows = csv.reader(f)
+    headers = next(rows)
+    for row in rows:
+        try:
+            total_cost += int(row[1])*float(row[2])
+        except ValueError:
+            print("Couldn't Parse", row)
     return(total_cost)
 
 cost = portfolio_cost('data/portfolio.csv')
