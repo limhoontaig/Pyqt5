f87b9b906da85c7597c564b3ffae35fcc7dc2cfb
limhoontaig <limhoontaig@gmail.com>
2021-05-10 19:51:10 +0900

    method change using csv module

--- work/exam_01_30.py
+++ work/exam_01_30.py
@@ -1,14 +1,17 @@
+import csv
+
+
 def portfolio_cost(filename):
+    ''' computes a total cost(shares*price) of a portfolio file'''
     total_cost = 0
 
-    with open('data/portfolio.csv', 'rt') as f:
-        headers = next(f).split(',')
-        for line in f:
-            row = line.split(',')
-            try:
-                total_cost += int(row[1])*float(row[2])
-            except ValueError:
-                print("Couldn't Parse", line)
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            nshares = int(row[1])
+            price = float(row[2])
+            total_cost += nshares * price
         return total_cost
 
 total_cost = portfolio_cost('data/portfolio.csv')
