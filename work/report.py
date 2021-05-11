4f3f5d46e4f2f0fabb305aafa7590e09674bd6dc
limhoontaig <limhoontaig@gmail.com>
2021-05-11 21:18:01 +0900

    portfolio list and price def 생성

--- work/report.py
+++ work/report.py
@@ -1,27 +1,56 @@
-# C:\source\PyQt5\work\pcost.py
+# C:\source\PyQt5\work\report.py
 
 import csv
 
 def read_portfolio(filename):
-
+    '''
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price.
+    '''
     portfolio = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
 
-    f = open(filename)
-    rows = csv.reader(f)
-    headers = next(rows)
-    for row in rows:
-        holding = (row[0], int(row[1]), float(row[2]))
-        portfolio.append(holding)
-    return(portfolio)
+        for row in rows:
+            stock = {
+                 'name'   : row[0],
+                 'shares' : int(row[1]),
+                 'price'   : float(row[2])
+            }
+            portfolio.append(stock)
 
-def total_cost(portfolio):
-    
-    total = 0 
-    for name, shares, price in portfolio:
-        total += shares * price
-    return total
+    return portfolio
 
+def read_prices(filename):
+    '''
+    Read a CSV file of price data into a dict mapping names to prices.
+    '''
+    prices = {}
+    with open(filename) as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+
+    return prices
 
 portfolio = read_portfolio('data/portfolio.csv')
-total = total_cost(portfolio)
-print(total)
+prices    = read_prices('data/prices.csv')
+
+# Calculate the total cost of the portfolio
+total_cost = 0.0
+for s in portfolio:
+    total_cost += s['shares']*s['price']
+
+print('Total cost', total_cost)
+
+# Compute the current value of the portfolio
+total_value = 0.0
+for s in portfolio:
+    total_value += s['shares']*prices[s['name']]
+
+print('Current value', total_value)
+print('Gain', total_value - total_cost)
