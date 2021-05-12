7b5f340af21d5cf1ae59ad281a7068c93e13ba7a
limhoontaig <limhoontaig@gmail.com>
2021-05-12 19:37:52 +0900

    create a definition of a parse 0f csv file

--- work/fileparse_lht.py
+++ work/fileparse_lht.py
@@ -1,75 +1,22 @@
-# E:\source\pyqt5\work\report_lht.py
+# C:\source\PyQt5\work\fileparse_lht.py
 
 import csv
 
-def read_portfolio(filename):
+def parse_csv(filename):
     '''
-    Read a stock portfolio file into a list of dictionaries with keys
-    name, shares, and price.
+    Parse a CSV file into a list of records with type conversion
     '''
-    portfolio = []
     with open(filename) as f:
         rows = csv.reader(f)
+
+        # Read the file headers (if any)
         headers = next(rows)
+        
+        records = []
         for row in rows:
-            record = dict(zip(headers,row))
-            stock = {
-                'name' : record['name'],
-                'shares' : int(record['shares']),
-                'price' : float(record['price'])
-            }
-            portfolio.append(stock)
-    return portfolio
+            if not row:    # skip rows with no data
+                continue
+            record = dict(zip(headers, row))
+            records.append(record)
 
-def read_prices(filename):
-    '''
-    Read a csv file of price data into a dict mapping names to prices.
-    '''
-    prices = {}
-    with open(filename) as f:
-        rows = csv.reader(f)
-        for row in rows:
-            try:
-                prices[row[0]] = float(row[1])
-            except IndexError:
-                pass
-    return prices
-
-def make_report_data(portfolio,prices):
-    '''
-    Make a list of (name, shares, price, change) tuples given a portfolio list 
-    and a prices dictionary.
-    '''
-    rows = []
-    for stock in portfolio:
-        current_price = prices[stock['name']]
-        change = current_price - stock['price']
-        summary = (stock['name'], stock['shares'], current_price, change)
-        rows.append(summary)
-    return rows
-
-def print_report(reportdata):
-    '''
-    Print a nicely formated table form a list of (name, shares, price, change).
-    '''
-    headers = ('Name', 'Shares','Price','Change')
-    print('%10s %10s %10s %10s' % headers)
-    print((('-'*10)+' ')*len(headers))
-    for row in reportdata:
-        print('%10s %10d %10.2f %10.2f' % row)
-
-def portfolio_report(portfoliofile,pricefile):        
-    '''
-    Make a stock report given portfolio and price data files.
-    '''
-    # Read data files 
-    portfolio = read_portfolio(portfoliofile)
-    prices = read_prices(pricefile)
-
-    # Create the report data
-    report = make_report_data(portfolio,prices)
-
-    # Print it out
-    print_report(report)
-
-portfolio_report('data/portfolio.csv','data/prices.csv')
+    return records
