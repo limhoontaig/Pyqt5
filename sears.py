dc6e0d2dcc21b6e96ebcd9ceaea0fc7ab7243939
LimHoonTaig <limhoontaig@gmail.com>
2021-06-08 07:08:50 +0900

    class interim

--- sears.py
+++ sears.py
@@ -1,19 +1,32 @@
 # E:\source\pyqt5\sears.py
 
-bill_thickness = 0.11 * 0.001    # Meters (0.11 mm)
-sears_height   = 542             # Height (meters)
-num_bills      = 1
-tot_bills = 1
-day            = 1
 
-while tot_bills * bill_thickness < sears_height:
-    print(f'{day:>4d} {num_bills:>10d} {tot_bills:>10d} {tot_bills * bill_thickness:10.2f}')
-    day = day + 1
-    num_bills = num_bills * 2
-    tot_bills += num_bills
-print('---------------------------- ----------')
-print('Number of days             :', f'{day:>10d}')
-print('Number of bills of last day:', f'{num_bills:>10d}')
-print('Total Number of bills      :', f'{tot_bills:>10d}')
-print('Final height               :', f'{tot_bills * bill_thickness:>10.2f}')
-print('**************************** **********')
+class Sears:
+    def __init__(self):
+        self.bill_thickness = 0.11 * 0.001    # Meters (0.11 mm)
+        self.sears_height   = 542             # Height (meters)
+        self.num_bills      = 1
+        self.tot_bills = 1
+        self.day            = 1
+        self.sears_compute = []
+
+    def sears():
+        while self.tot_bills * self.bill_thickness < self.sears_height:
+            self.sears.compute.append(f'{day:>4d} {num_bills:>10d} {tot_bills:>10d} {tot_bills * bill_thickness:10.2f}')
+            day = day + 1
+            num_bills = num_bills * 2
+            tot_bills += num_bills
+        return self.sears_compute
+
+
+    def report(sears):
+        print('---------------------------- ----------')
+        print('Number of days             :', f'{day:>10d}')
+        print('Number of bills of last day:', f'{num_bills:>10d}')
+        print('Total Number of bills      :', f'{tot_bills:>10d}')
+        print('Final height               :', f'{tot_bills * bill_thickness:>10.2f}')
+        print('**************************** **********')
+
+calculation = Sears.sears()
+
+Sears.report(calculation)
